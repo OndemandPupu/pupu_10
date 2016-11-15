571dc6aeb9460b2b2095846081dfb41aab0cc282
leesh919292 <leesh919292@gmail.com>
2016-11-15 11:15:13 +0900

    change files

---
+++ src/m/controller/IndexController.java
@@ -0,0 +1,24 @@
+package m.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index.nhn")
+	public String welcome() {
+
+		return "t:index";
+	}
+	
+	@RequestMapping("/login.nhn")
+	public String loginView() {
+		return "login/loginView";
+	}
+	
+	@RequestMapping("/member.nhn")
+	public String memberView() {
+		return "login/memberView";
+	}
+}
